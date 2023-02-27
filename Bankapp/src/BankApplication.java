605055b9fa6c7ea891389b824270b9ac05d2b44d
Hugo Ohlsson <hugoohlsson02@hotmail.com>
2023-02-27 14:26:41 +0100

    jubb

--- Bankapp/src/BankApplication.java
+++ Bankapp/src/BankApplication.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 
 public class BankApplication {
 	static Bank bank = new Bank();
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		menu();
-		
-		int option = scan.nextInt(); 
+		test();
+		int option = scan.nextInt();
 		switch (option) {
 		case 1:
+			System.out.println("id:");
+			System.out.println(bank.findAccountsForHolder(scan.nextLong()));
 			break;
 		case 2:
+			System.out.println("namn:");
+			System.out.println(bank.findByPartofName(scan.next()));
 			break;
 		case 3:
+			System.out.println("konto:");
+			BankAccount account = bank.findByNumber(scan.nextInt());
+			System.out.println("belopp:");
+			double belopp = scan.nextDouble();
+			account.deposit(belopp);
+			System.out.println(account);
 			break;
 		case 4:
 			break;
@@ -27,11 +37,11 @@ public class BankApplication {
 			break;
 		case 9:
 			break;
+		default:
+			System.out.println("1");
 		}
 	}
-	
-	
-	
+
 	public static void menu() {
 		System.out.println("--------------------------------------------------------------");
 		System.out.println("1. Hitta konton för en viss kontoinnehavare");
@@ -43,19 +53,20 @@ public class BankApplication {
 		System.out.println("7. Ta bort konto");
 		System.out.println("8. Skriv ut bankens alla konton");
 		System.out.println("9. Avsluta");
-			}
-	
+		System.out.println("Val: ");
+	}
+
 	public static void test() {
-			bank.addAccount("Erik Johansson", 7208151245L);
-			bank.addAccount("Anna Andersson", 9307023397L);
-			bank.addAccount("Johan Svensson", 8102107584L);
-			bank.addAccount("Sofia Lundqvist", 6408279876L);
-			bank.addAccount("Jesus Gustavsson", 7812033218L);
-			bank.addAccount("Linda Nilsson", 9503289021L);
-			bank.addAccount("Dickbert Carlsson", 8206296643L);
-			bank.addAccount("Sara Bergström", 8707225213L);
-			bank.addAccount("Kristina Söderberg", 9201218765L);
-			bank.addAccount("Magnus Andersson", 6909154321L);
-		
+		bank.addAccount("Erik Johansson", 720815124);
+		bank.addAccount("Anna Andersson", 9307023397L);
+		bank.addAccount("Johan Svensson", 8102107584L);
+		bank.addAccount("Sofia Lundqvist", 6408279876L);
+		bank.addAccount("Jesus Gustavsson", 7812033218L);
+		bank.addAccount("Linda Nilsson", 9503289021L);
+		bank.addAccount("Dickbert Carlsson", 8206296643L);
+		bank.addAccount("Sara Bergström", 8707225213L);
+		bank.addAccount("Kristina Söderberg", 9201218765L);
+		bank.addAccount("Magnus Andersson", 6909154321L);
+
 	}
 }
